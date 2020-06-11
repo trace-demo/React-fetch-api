a9b9dfde4f52509ae1c7b94ecbc2af591b0703fe
polarpolarbear <2788448286@qq.com>
2020-06-11 12:47:48 -0400

    Done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            term: '',
+            img: ''
+        };
+    }
+
+    onChange = (event) => {
+        this.setState({
+            term: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const api_key = 'dc6zaTOxFJmzC'
+        const url = `http://api.giphy.com/v1/gifs/search?q=${this.state.term}&api_key=${api_key}`
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=>this.setState({
+            term:'',
+            img:data.data[0].images.fixed_height.url 
+        }))
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <form onSubmit={this.handleSubmit}>
+                    <input value={this.state.term} onChange={this.onChange} />
+                    <button>SEARCH !</button>
+                </form>               
+                <img src={this.state.img} height ="200" alt={this.state.term} />
+            </div>
+        )
+    }
 }
+
 
 export default App;
